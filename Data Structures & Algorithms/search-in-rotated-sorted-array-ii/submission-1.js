130ffb99f011f655dfcb52ed30444b583392bc0e
minal6066 <71309796+minal6066@users.noreply.github.com>
2026-06-26 14:20:08 +0530

    Add: search-in-rotated-sorted-array-ii - submission-1

---
+++ Data Structures & Algorithms/search-in-rotated-sorted-array-ii/submission-1.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {boolean}
+     */
+    search(nums, target) {
+        let l=0, r=nums.length-1;
+        while(l<=r){
+            let m = Math.floor((l+r)/2);
+            if(nums[m] == target){
+                return true;
+            } 
+
+            if (nums[l] < nums[m]) {
+                // Left portion
+                if (nums[l] <= target && target < nums[m]) {
+                    r = m - 1;
+                } else {
+                    l = m + 1;
+                }
+            } else if (nums[l] > nums[m]) {
+                // Right portion
+                if (nums[m] < target && target <= nums[r]) {
+                    l = m + 1;
+                } else {
+                    r = m - 1;
+                }
+            } else {
+                l++;
+            }
+        }
+        return false;
+    }
+}
